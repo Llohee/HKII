484f6430e9bbf5bcbd5e065d3ffa6bfe5f91ea3c
Nguyễn Đức Giang <nggiang141@gmail.com>
2023-01-14 22:30:02 +0700

    learn reactjs lesson 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-task">
+        <input name='task' type="text" placeholder='Type your task here!' className="input-task"/>
+        <div className='row'>
+        <input name='too-task' type="radio" id='clean-up'/>
+        <label htmlFor='clean-up'>Clean up bathroom</label>
+        </div>
+        <div className='row'>
+        <input name='too-task' type="radio" id='bs-milk'/>
+        <label htmlFor='clean-up'>Buy some milk</label>
+        </div><div className='row'>
+        <input name='too-task' type="radio" id='jogging'/>
+        <label htmlFor='clean-up'>Jogging</label>
+        </div><div className='row'>
+        <input name='too-task' type="radio" id='learn-react'/>
+        <label htmlFor='clean-up'>Learn React</label>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="left">5 tasks left!</div>
+        <div className="right">MindX todolist</div>
+      </div>
     </div>
   );
 }
